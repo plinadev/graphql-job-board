6d3341368074e625e455b9bab478bad6260b4857
plinadev <pavlenkopolina415@gmail.com>
2025-09-29 12:14:53 +0300

    Initial project setup

---
+++ server/resolvers.js
@@ -0,0 +1,16 @@
+import { getJobs } from "./db/jobs.js";
+import { getCompany } from "./db/companies.js";
+
+export const resolvers = {
+  Query: {
+    jobs: () => getJobs(),
+  },
+  Job: {
+    company: (job) => getCompany(job.companyId),
+    date: (job) => toIsoDate(job.createdAt),
+  },
+};
+
+function toIsoDate(value) {
+  return value.slice(0, "yyy-mm-dd".length);
+}
